8b48c69a1cfb072f435173a5f3a209feec8f02a0
username <barasamica@gmail.com>
2024-02-24 01:13:04 +0300

    Styled the Landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import engineer from '../assets/engineer.jpg'
+import './LandingPage.css'
 
 const Landing = () => {
   const [showWelcome, setShowWelcome] = useState(false);
