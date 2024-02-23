f252baa409648e218aa0e94b8d4b11349aa7dbbc
username <barasamica@gmail.com>
2024-02-23 20:41:30 +0300

    Created the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -12,8 +12,26 @@ const LandingPage = () => {
     }, []);
 
   return (
-    <div>
-      
+    <div className='landing-container'>
+        <div className='landimg-content'>
+            <h1 className={`welcome-message ${showWelcome ? 'show' : ''}`}>
+               Welcome To My Portfolio
+            </h1>
+            <p>
+                Hi I'm Micah Barasa, a passionate FullStack Software Engineer with a love for building robust and scalable applications.
+            </p>
+            <div className='call-to-action'>
+                <p>Explore my work and skills below</p>
+            </div>
+            <div className='action-buttons'>
+                <Link to='/projects' className='explore button'>
+                    Explore Projects
+                </Link>
+                <Link to='about' className='learn-more-button'>
+                    Learn More About Me
+                </Link>
+            </div>
+        </div>
     </div>
   )
 }
